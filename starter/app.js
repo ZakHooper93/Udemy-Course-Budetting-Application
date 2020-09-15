26208a04b01536699e3bef2974c01cd12b3043e6
Zak Hooper <68293009+ZakHooper93@users.noreply.github.com>
2020-09-15 16:41:29 +0100

    Update app.js

--- starter/app.js
+++ starter/app.js
@@ -1,26 +1,27 @@
+//BUDGET CONTROLLER
 var budgetController = (function () {
-    var x = 23;
-
-    var add = function (a) {
-        return x + a;
-    };
-    return {
-        publicTest: function (b) {
-            return add(b);
-        },
-    };
+    //Some Code
 })();
 
+//UI CONTROLLER
 var UIController = (function () {
-    //Some code
+    //Some Code
 })();
 
+//GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function () {
+        // 1. Get the field input data.
+        // 2. Add the item to the budget controller.
+        // 3. Add the item to the UI.
+        // 4. Calculate the budget.
+        // 5. Display the budget on the UI.}
 
-    return {
-        anotherPublic: function () {
-            console.log(z);
-        },
+        document.querySelector(".add__btn").addEventListener("click", function () {});
+        document.addEventListener("keypress", function (event) {
+            if (event.keycode === 13 || event.which === 13) {
+                console.log("Enter was pressed");
+            }
+        });
     };
 })(budgetController, UIController);
