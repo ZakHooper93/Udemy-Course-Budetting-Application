26750e60c4fe9ca0936307f9211b00e24e144c90
Zak Hooper <68293009+ZakHooper93@users.noreply.github.com>
2020-09-24 16:07:05 +0100

    UI now reflects changes when removing an item

--- starter/app.js
+++ starter/app.js
@@ -153,6 +153,12 @@ var UIController = (function () {
             //Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
         },
+
+        deleteListItem: function (selectorID) {
+            var el = document.getElementById(selectorID)
+            el.parentNode.removeChild(el)
+
+        },
         //A function to clear the input fields once an entry has been made.
         clearFields: function () {
             var fields;
@@ -251,8 +257,10 @@ var controller = (function (budgetCtrl, UICtrl) {
             //1. Delete item from the data structure.
             budgetCtrl.deleteItem(type, ID);
             //2. Delete the item from the user interface.
-
+            UICtrl.deleteListItem(itemID);
             //3. Update and show the new budget.
+            updateBudget()
+
         }
     }
     //A test initialisation function to ensure it's all loaded. To be called at the end of the code.
